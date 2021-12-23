15516aa20f62ffbb28bb91add3d6e38e5910deb9
Kikidragon <keekdragon@icloud.com>
2021-12-23 12:17:25 -0600

    test

---
+++ fav_number.cpp
@@ -0,0 +1,12 @@
+//
+// Created on 12/23/2021.
+//
+# include <iostream>
+
+int main(){ //begins execution
+    int favorite_number;  //creates variable as int
+    std::cout<<"Enter your favorite number between 1 and 100:";  //print the message
+    std::cin>>favorite_number;  //takes input for favorite number and stores
+    std::cout<<"Amazing!!! That's my favorite number too!"<<std::endl;  //prints out the message
+    return 0;
+}
